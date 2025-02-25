204bb0f8fe063be6eaf9050e8f3c86dee46f7984
squld <squld@kurusugawa.jp>
2025-02-25 06:24:44 +0000

    Refactor webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,56 @@
 'use strict';
 
 const path = require('path');
+const { merge } = require('webpack-merge');
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
-/** @type WebpackConfig */
-const electronConfig = {
-  target: 'node',
-  mode: 'none',
-
+const commonConfig = {
   entry: './src/extension.ts',
+  mode: 'none',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'extension.js',
     libraryTarget: 'commonjs2'
   },
   externals: {
     vscode: 'commonjs vscode'
   },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ]
+        use: 'ts-loader'
       }
     ]
   },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+};
+
+const electronConfig = merge(commonConfig, {
+  target: 'node',
+  output: { filename: 'extension.js' },
   devtool: 'nosources-source-map',
   infrastructureLogging: {
     level: "log", // enables logging required for problem matchers
   },
-};
+});
 
-/** @type WebpackConfig */
-const browserConfig = {
+const browserConfig = merge(commonConfig, {
   target: 'webworker',
-  mode: 'none',
-
-  entry: './src/extension.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'web-extension.js',
-    libraryTarget: 'commonjs2'
-  },
-  externals: { vscode: 'commonjs vscode' },
+  output: { filename: 'web-extension.js' },
   resolve: {
     mainFields: ["module", "main"],
-    extensions: ['.ts', '.js'],
     fallback: {
       assert: false,
       os: false,
-      path: require.resolve("path-browserify"),
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ]
-      }
-    ]
+      path: require.resolve("path-browserify")
+    }
   },
   devtool: 'source-map',
-};
+});
 
 module.exports = [electronConfig, browserConfig];
